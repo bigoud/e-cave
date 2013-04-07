dfe9cb71a3c837ee9b82fc0e77ed0a6d0b305f6c
nicko <nicko466@gmail.com>
2013-04-07 12:17:19 +0200

    Consult les différentes bouteilles présente dans la base de données OK avec message d'erreur si l'utilisateur n'est pas connecté à internet...

--- nfcPhoneGap/assets/www/js/consult.js
+++ nfcPhoneGap/assets/www/js/consult.js
@@ -1,6 +1,17 @@
 
 
 function request() {
+	var Inventory;
 	href = 'https://api.mongolab.com/api/1/databases/heroku_app14597085/collections/winedatabases?apiKey=kP7a0LRQmPijRkR9AV580c33FRq4kvfK';
-	document.write(href);
+	$.get(href, function(Inventory) {
+			var InventoryTab = jQuery.makeArray(Inventory);
+			var display = document.getElementById("bottleInventory");
+			var p = document.createElement('p');
+			for (i =0 ; i<InventoryTab.length;i++){
+				p.innerHTML += "Type de vin : " + InventoryTab[i].typeDeVin  + "<br> annee :" + InventoryTab[i].annee + "<br> domaine : " + InventoryTab[i].domaine+"<br><br>"; 
+			}
+			display.appendChild(p);	
+	},
+	"json")
+	.fail(function(){ alert (" Attention Vous n'êtes pas connecté à Internet ");});
 };
